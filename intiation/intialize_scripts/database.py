f3875d1a3fee1d80355467683badc2cfc4e76849
kundhan <kundhan1997@gmail.com>
2025-09-17 22:11:55 +0530

    database connection

---
+++ intiation/intialize_scripts/database.py
@@ -0,0 +1,41 @@
+import os
+import psycopg2
+import json
+from psycopg2.extras import Json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Register the json adapter
+psycopg2.extensions.register_adapter(dict, Json)
+
+def get_db_connection():
+    """Establishes a connection to the PostgreSQL database."""
+    conn = psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
+    return conn
+
+def execute_query(sql, params=None):
+    """Executes a given SQL query with optional parameters.
+    Handles connection, cursor, and transaction management.
+    """
+    conn = get_db_connection()
+    cur = conn.cursor()
+    try:
+        if params:
+            cur.execute(sql, params)
+        else:
+            cur.execute(sql)
+        conn.commit()
+    except Exception as e:
+        conn.rollback()
+        print(f"An error occurred: {e}")
+        raise
+    finally:
+        cur.close()
+        conn.close()
