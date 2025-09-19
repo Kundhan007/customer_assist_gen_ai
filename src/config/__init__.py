615f206ad1fa47e0461c1712ce4498cd1c917e00
kundhan <kundhan1997@gmail.com>
2025-09-20 00:43:05 +0530

    load env

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=env_path)
 
 # Database configuration (can be accessed via os.getenv)
 DB_NAME = os.getenv("DB_NAME")
