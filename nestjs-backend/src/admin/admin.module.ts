c11fe563a4e9892e46d4ee81bb025c113169cf43
kundhan <kundhan1997@gmail.com>
2025-09-20 14:12:46 +0530

    feat: update core application structure and user management

--- nestjs-backend/src/admin/admin.module.ts
+++ nestjs-backend/src/admin/admin.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { HttpModule } from '@nestjs/axios';
+import { UsersModule } from '../users/users.module';
+import { PoliciesModule } from '../policies/policies.module';
+import { ClaimsModule } from '../claims/claims.module';
+import { PremiumModule } from '../premium/premium.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    UsersModule,
+    PoliciesModule,
+    ClaimsModule,
+    PremiumModule,
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
