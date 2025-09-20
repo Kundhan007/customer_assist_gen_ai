a5ff9f154091bb41552a13bf21c8b8ec8c6f6d94
kundhan <kundhan1997@gmail.com>
2025-09-20 16:55:24 +0530

    refactor: reorganize python orchestrator structure and update nestjs package

--- python-orchestrator/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Python Vectorization Orchestrator
-
-A lightweight, configurable text vectorization service using sentence transformers.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Vectorization Team"
